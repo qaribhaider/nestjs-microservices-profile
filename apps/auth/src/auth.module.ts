876e24ef663bd76031c05cc338a2a3246210302f
Syed Qarib <qaribhaider@gmail.com>
2024-07-27 18:25:32 +0200

    Add joi for config validation

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+      }),
     }),
   ],
   controllers: [AuthController],

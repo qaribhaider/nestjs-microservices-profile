fa31e5a7d62d20292684aab42f6f0c4e06d4f03f
Syed Qarib <qaribhaider@gmail.com>
2024-07-28 00:01:58 +0200

    Refactor to have auth deal with the auth logic

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import { UsersService } from './users/users.service';
 import { CreateUserDto } from './users/dto/create-user.dto';
 import { LoginUserDto } from './users/dto/login-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -15,11 +16,38 @@ export class AuthService {
   }
 
   async registerUser(createUser: CreateUserDto) {
-    return this.usersService.create(createUser);
+    let user = null;
+    try {
+      user = await this.usersService.getUserByEmail(createUser.email);
+    } catch (error) {
+      // We generally get an error here when the user doesn't exist
+      // so we can continue to create the user
+    }
+
+    if (user) {
+      throw new UnprocessableEntityException('Email already exists.');
+    }
+
+    return this.usersService.create({
+      ...createUser,
+      password: await bcrypt.hash(createUser.password, 10),
+    });
   }
 
   async loginUser(loginUser: LoginUserDto) {
-    // TODO: implement login authentication via JWT
-    return this.usersService.verifyUser(loginUser.email, loginUser.password);
+    return this.verifyUserLogin(loginUser.email, loginUser.password);
+  }
+
+  async verifyUserLogin(email: string, password: string) {
+    const user = await this.usersService.getUserByEmail(email).catch((error) => {
+      throw new NotFoundException(error, 'User not found by the provided email address');
+    });
+
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+    return user;
   }
 }
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,41 +1,20 @@
-import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
-    await this.validateIfUserAlreadyRegistered(createUserDto);
     return this.prismaService.user.create({
-      data: {
-        ...createUserDto,
-        password: await bcrypt.hash(createUserDto.password, 10),
-      },
+      data: createUserDto,
     });
   }
 
-  private async validateIfUserAlreadyRegistered(createUserDto: CreateUserDto) {
-    try {
-      await this.prismaService.user.findFirstOrThrow({
-        where: { email: createUserDto.email },
-      });
-    } catch (err) {
-      return;
-    }
-    throw new UnprocessableEntityException('Email already exists.');
-  }
-
-  async verifyUser(email: string, password: string) {
-    const user = await this.prismaService.user.findFirstOrThrow({
+  async getUserByEmail(email: string) {
+    return await this.prismaService.user.findFirstOrThrow({
       where: { email },
     });
-    const passwordIsValid = await bcrypt.compare(password, user.password);
-    if (!passwordIsValid) {
-      throw new UnauthorizedException('Credentials are not valid.');
-    }
-    return user;
   }
 }

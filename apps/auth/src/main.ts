e8c4b9d52fcd26cbe733e4986695258bdfbd16b4
Syed Qarib <qaribhaider@gmail.com>
2024-07-28 09:06:17 +0200

    Enable comms for auth service via TCP

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('TCP_PORT'),
+    },
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  await app.startAllMicroservices();
   await app.listen(configService.get('HTTP_PORT'), () => {
     Logger.log(`Auth service started on port ${configService.get('HTTP_PORT')}`);
   });

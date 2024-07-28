8b970b72e9388ea1b00084a7dcc4115a1da8a63d
Syed Qarib <qaribhaider@gmail.com>
2024-07-28 09:23:26 +0200

    Implement profile microservice

--- apps/profile/src/app.module.ts
+++ apps/profile/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
